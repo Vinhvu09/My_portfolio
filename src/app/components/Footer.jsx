278d6b64a2527e23755a3c2bac4ce6171ee81917
Vinhgitlap99 <vqv20115@gmail.com>
2023-12-04 15:10:02 +0700

    first commit

---
+++ src/app/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span>SEE YOU AGAIN</span>
+        <p className="text-slate-600">Thank you for coming</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
